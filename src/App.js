ced736358c5e1844491e2596c5a8023339d3987c
Samuel Orodi <orodi.samuel008@gmail.com>
2023-08-09 14:01:04 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const movies = <h2>Pokemon</h2>;
+  const element = <a href="https://ww.google.com">Link to a website</a>;
+
+  return (
+    <>
+      <div>
+        <h1>This is JSX</h1>
+        <p>Thank you for learning JSX!!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
